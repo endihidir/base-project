96c83955b299e9cb011acc7f412055d45d5583e2
Endi Hıdır <32939545+endihidir@users.noreply.github.com>
2025-10-01 17:31:12 +0300

    QuitInvokeUtil has been further optimized

--- Assets/UnityBase/Runtime/Scripts/Systems/SaveSystem/QuitInvokeUtil.cs
+++ Assets/UnityBase/Runtime/Scripts/Systems/SaveSystem/QuitInvokeUtil.cs
@@ -1,51 +1,64 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace UnityBase.SaveSystem
 {
     public static class QuitInvokeUtil
     {
         private static bool _cached;
-        private static readonly List<MethodInfo> StaticMethods = new();
-        private static readonly Dictionary<Type, List<MethodInfo>> InstanceMethodsByType = new();
+        
+        private static readonly List<Action> _staticInvokes = new();
+        private static readonly Dictionary<Type, List<Action<UnityEngine.Object>>> _instanceInvokesByDeclType = new();
+        
+        private static readonly string[] _allowedAsmPrefixes =
+        {
+            "Assembly-CSharp",
+            "UnityBase",
+            "Game"
+        };
 
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
         public static void Init()
         {
             CacheIfNeeded();
         }
-        
+
         public static void InvokeAll()
         {
             CacheIfNeeded();
             
-            foreach (var m in StaticMethods)
-                SafeInvoke(m, null);
-            
-            if (InstanceMethodsByType.Count > 0)
+            for (int i = 0; i < _staticInvokes.Count; i++)
             {
-                var all = Resources.FindObjectsOfTypeAll<UnityEngine.Object>();
-                if (all != null && all.Length > 0)
-                {
-                    foreach (var obj in all)
-                    {
-                        var t = obj.GetType();
-                        foreach (var kv in InstanceMethodsByType)
-                        {
-                            var declType = kv.Key;
-                            if (!declType.IsAssignableFrom(t)) continue;
+                SafeInvoke(_staticInvokes[i]);
+            }
+            
+            foreach (var kv in _instanceInvokesByDeclType)
+            {
+                var declType = kv.Key;
+                var invokes = kv.Value;
 
-                            var list = kv.Value;
-                            foreach (var m in list)
-                                SafeInvoke(m, obj);
-                        }
+                var instances = FindAllOfType(declType);
+                if (instances == null || instances.Count == 0) continue;
+
+                for (int i = 0; i < instances.Count; i++)
+                {
+                    var obj = instances[i];
+                    for (int j = 0; j < invokes.Count; j++)
+                    {
+                        SafeInvoke(invokes[j], obj);
                     }
                 }
             }
-            
-            try { PlayerPrefs.Save(); } catch (Exception e) { Debug.LogException(e); }
+
+            try { PlayerPrefs.Save(); }
+            catch (Exception e) { Debug.LogException(e); }
         }
 
         private static void CacheIfNeeded()
@@ -53,49 +66,117 @@ namespace UnityBase.SaveSystem
             if (_cached) return;
             _cached = true;
 
+#if UNITY_EDITOR
+            var methods = TypeCache.GetMethodsWithAttribute<InvokeOnQuitAttribute>();
+            
+            for (int i = 0; i < methods.Count; i++)
+            {
+                TryRegisterMethod(methods[i]);
+            }
+#else
+
             foreach (var asm in AppDomain.CurrentDomain.GetAssemblies())
             {
+                if (!IsAllowedAssembly(asm)) continue;
+
                 Type[] types;
                 try { types = asm.GetTypes(); }
                 catch { continue; }
 
-                foreach (var t in types)
+                for (int ti = 0; ti < types.Length; ti++)
                 {
-                    var flags = BindingFlags.Public | BindingFlags.NonPublic |
-                                BindingFlags.Static | BindingFlags.Instance;
-
+                    var t = types[ti];
                     MethodInfo[] methods;
-                    try { methods = t.GetMethods(flags); }
+                    try
+                    {
+                        methods = t.GetMethods(BindingFlags.Public | BindingFlags.NonPublic |
+                                               BindingFlags.Static | BindingFlags.Instance |
+                                               BindingFlags.DeclaredOnly);
+                    }
                     catch { continue; }
 
-                    foreach (var m in methods)
+                    for (int mi = 0; mi < methods.Length; mi++)
                     {
-                        if (m.GetCustomAttribute<InvokeOnQuitAttribute>() == null)
-                            continue;
-
-                        if (!IsSupportedSignature(m))
-                            continue;
-
-                        if (m.IsStatic)
-                        {
-                            StaticMethods.Add(m);
-                        }
-                        else
-                        {
-                            if (!InstanceMethodsByType.TryGetValue(t, out var list))
-                                InstanceMethodsByType[t] = list = new List<MethodInfo>();
-                            list.Add(m);
-                        }
+                        var m = methods[mi];
+                        if (m.GetCustomAttribute<InvokeOnQuitAttribute>() == null) continue;
+                        TryRegisterMethod(m);
                     }
+                }
+            }
+#endif
+        }
+
+        private static void TryRegisterMethod(MethodInfo m)
+        {
+            if (m.ReturnType != typeof(void) || m.GetParameters().Length != 0)
+                return;
+
+            if (m.IsStatic)
+            {
+                try
+                {
+                    var d = (Action)Delegate.CreateDelegate(typeof(Action), m);
+                    _staticInvokes.Add(d);
+                }
+                catch
+                {
+                    _staticInvokes.Add(() => m.Invoke(null, null));
+                }
+            }
+            else
+            {
+                var declType = m.DeclaringType;
+                if (declType == null) return;
+
+                if (!_instanceInvokesByDeclType.TryGetValue(declType, out var list))
+                {
+                    list = new List<Action<UnityEngine.Object>>();
+                    _instanceInvokesByDeclType[declType] = list;
+                }
+
+                try
+                {
+                    var del = m.CreateDelegate(typeof(Action<>).MakeGenericType(declType));
+                    list.Add((target) => del.DynamicInvoke(target));
+                }
+                catch
+                {
+                    list.Add((target) => m.Invoke(target, null));
                 }
             }
         }
 
-        private static bool IsSupportedSignature(MethodInfo m) => m.ReturnType == typeof(void) && m.GetParameters().Length == 0;
-
-        private static void SafeInvoke(MethodInfo m, object target)
+        private static bool IsAllowedAssembly(Assembly asm)
         {
-            try { m.Invoke(target, null); }
+            var n = asm.GetName().Name;
+            for (int i = 0; i < _allowedAsmPrefixes.Length; i++)
+            {
+                if (n.StartsWith(_allowedAsmPrefixes[i], StringComparison.Ordinal))
+                    return true;
+            }
+            return false;
+        }
+
+        private static List<UnityEngine.Object> FindAllOfType(Type t)
+        {
+#if UNITY_2023_1_OR_NEWER
+            var arr = UnityEngine.Object.FindObjectsByType(t, FindObjectsInactive.Include, FindObjectsSortMode.None);
+            return arr?.ToList();
+#else
+            var arr = Resources.FindObjectsOfTypeAll(t);
+            return arr?.ToList();
+#endif
+        }
+
+        private static void SafeInvoke(Action a)
+        {
+            try { a(); }
+            catch (Exception e) { Debug.LogException(e); }
+        }
+
+        private static void SafeInvoke(Action<UnityEngine.Object> a, UnityEngine.Object target)
+        {
+            try { a(target); }
             catch (Exception e) { Debug.LogException(e); }
         }
     }
